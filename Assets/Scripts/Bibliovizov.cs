ca7b5a730bfb03eb783f9195836c54d5d77054a3
VladlenOBB <34621870+VladlenOBB@users.noreply.github.com>
2017-12-17 16:42:29 +0300

    Add files via upload

---
+++ Assets/Scripts/Bibliovizov.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class Bibliovizov : MonoBehaviour {
+
+	void Start () {
+		
+	}
+    [DllImport("Biblioteka")]
+    private static extern int answer();
+
+	void Update () {
+        Debug.Log(answer());
+	}
+
+    void OnGUI()
+    {
+        float fps = 1.0f / Time.deltaTime + answer();
+        GUILayout.Label("FPS + 42 = " + fps);
+    }
+}
---
+++ Assets/Scripts/transformation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformation : MonoBehaviour {
+    public float minimum = 10.0F;
+    public float maximum = 20.0F;
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.localScale = new Vector3(Mathf.PingPong(Time.time*1000, maximum - minimum) + minimum, Mathf.PingPong(Time.time * 1000, maximum - minimum) + minimum,Mathf.PingPong(Time.time * 1000, maximum - minimum) + minimum);
+
+    }
+}
